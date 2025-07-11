83643b52ce7cf60efb4119d6d836378b6ac797b9
Jeremy Lincoln <bossco28@gmail.com>
2025-07-11 12:53:02 -0400

    Named Routes

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { route } from '../../../vendor/tightenco/ziggy/src/js';
+
 // import { Link, Head } from '@inertiajs/vue3';
 </script>
 
@@ -8,8 +10,8 @@
             <nav class="flex items-center justify-between p-4">
                 <h1>{{ $page.props.auth.user }}</h1>
                 <div class="space-x-6">
-                    <Link  href="/">Home</Link>
-                    <Link  href="/about">About</Link>
+                    <Link  :href="route('home')">Home</Link>
+                    <Link  :href="route('about')">About</Link>
                 </div>
             </nav>
         </header>
