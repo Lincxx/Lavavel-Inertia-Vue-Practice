133742c8eafc6ef66721caf74fc51ffb658b48ab
Jeremy Lincoln <bossco28@gmail.com>
2025-07-11 08:42:48 -0400

    Link & Head elements

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,23 @@
+<script setup>
+// here we can override the layout
+// import Layout from '../Layouts/demo.vue';
+
+// defineOptions({
+//     layout: Layout
+// })
+
+// these could be imported as a global component
+// import { Head } from '@inertiajs/vue3';
+</script>
+
 <template>
-    <div>
-        <h1>Hello</h1>
-    </div>
+    <!-- <Head>
+        <title>Home </title>
+        <meta type="description" content="My home description" head-key="description">
+    </Head> -->
+
+    <Head title="| Home" />
+        
+    
+    <h1 class="text-3xl font-bold ">Hello</h1>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+// Route::get('/about', function () {
+//     return inertia('About', ['user' => 'Mike']);
+// });
+// works the same as above
+Route::inertia('/about', 'About', ['user' => 'Mike']);
